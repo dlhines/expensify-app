104028ecee5c04e0f92d80cecefbc9a10219d363
Daniel Hines <dlhines.net@gmail.com>
2018-05-21 17:44:28 -0500

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,64 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry:'./src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/,
-        },{
-            test:/\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ]
-        }]
-    },
-    devtool: "cheap-module-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true // This is for react-router-dom. It tells rrd to use / as a prefix for all pages
+module.exports = (env) =>  {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry:'./src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },{
+                test:/\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true // This is for react-router-dom. It tells rrd to use / as a prefix for all pages
+        }
     }
 };
+// module.exports = {
+//     entry:'./src/app.js',
+//     output: {
+//         path: path.join(__dirname, 'public'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//         },{
+//             test:/\.s?css$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader',
+//             ]
+//         }]
+//     },
+//     devtool: "cheap-module-source-map",
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true // This is for react-router-dom. It tells rrd to use / as a prefix for all pages
+//     }
+// };
